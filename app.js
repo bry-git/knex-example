43ef3f2c192a1694a6a4c004c6fc4e892871dfe2
bryson crandall <bryson.crandall@yahoo.com>
2021-08-12 13:54:54 -0700

    initial

---
+++ app.js
@@ -0,0 +1,32 @@
+// app.js
+const express = require('express');
+var bodyParser = require('body-parser')
+const app = express();
+const PORT = process.env.PORT || 3000;
+const knex = require('knex')(require('./knexfile')[process.env.NODE_ENV]);
+
+app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+
+app.get('/movies', function(req, res) {
+  knex
+    .select('*')
+    .from('movies')
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'The data you are looking for could not be found. Please try again'
+      })
+    );
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT}`);
+});
